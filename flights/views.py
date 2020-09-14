779cf18f65b842d27d053e25d778519f688d58ef
jawish-a@outlook.com <jawish-a@outlook.com>
2020-09-14 11:35:31 +0300

    completed the task

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
+
+from rest_framework.generics import ListAPIView
 from .models import Flight, Booking
+from .serializers import FlightListSerializer, BookingListSerializer
+
+import datetime
+
+class FlightsList(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightListSerializer
+
+class Bookings(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=datetime.datetime.now())
+    serializer_class = BookingListSerializer
